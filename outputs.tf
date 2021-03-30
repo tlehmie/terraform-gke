1e1d1ca0dbbc9487b9496c5542b0d9897874a29b
kserge2001 <kserge2001@yahoo.fr>
2021-03-30 15:13:09 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "beacon_url" {
-  value = "${kubernetes_service.beacon.load_balancer_ingress.0.ip}:${kubernetes_service.beacon.spec.0.port.0.port}"
+output "ap-deploy-demo_url" {
+  value = "${kubernetes_service.app-deploy-service.load_balancer_ingress.0.ip}:${kubernetes_service.app-deploy-service.spec.0.port.0.port}"
 }
